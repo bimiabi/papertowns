f2a09abf3da66132c8722155b7dc964493b25153
bimiabi <bimi_abi@hotmail.com>
2015-12-08 18:46:47 +0200

    test

--- db/migrate/20151129085102_add_image_product_details_to_product.rb
+++
@@ -1,6 +0,0 @@
-class AddImageProductDetailsToProduct < ActiveRecord::Migration
-  def change
-    add_column :products, :asset_uid, :string
-    add_column :products, :asset_name, :string
-  end
-end
